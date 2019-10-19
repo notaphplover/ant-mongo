1db1eb246baf5d76dd94f92da47547eae9988d14
Roberto Pintos López <roberto@secture.com>
2019-10-19 10:14:42 +0200

    Renamed mongo model class.

--- src/model/AntMongoModel.ts
+++ src/model/AntMongoModel.ts
@@ -12,7 +12,7 @@ const defaultMapping: IMappingStrategy = {
   fields: [ANT_MONGO_DEFAULT_ID],
 };
 
-export class MongoModel extends Model implements IAntMongoModel {
+export class AntMongoModel extends Model implements IAntMongoModel {
   /**
    * Name of the MongoDB collection mapped to this model.
    */
