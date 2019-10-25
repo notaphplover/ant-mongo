1825cfc09d6519e656ba8696267b12426196bb60
Roberto Pintos López <roberto@secture.com>
2019-10-25 19:34:35 +0200

    Updated library to support the last version of @antjs/ant-js.

--- src/test/AllTest.ts
+++ src/test/AllTest.ts
@@ -1,7 +1,7 @@
-import { ITest } from '@antjs/ant-js/src/testapi/api/ITest';
+import { Test } from '@antjs/ant-js/build/testapi/api/test';
 import { AntMongoModelTest } from './model/AntMongoModelTest';
 
-export class AllTest implements ITest {
+export class AllTest implements Test {
   public performTests(): void {
     new AntMongoModelTest().performTests();
   }
