452737ba97f226b403d67f90625823b3f85409c6
Roberto Pintos López <roberto@secture.com>
2019-11-17 20:13:03 +0100

    DB connection promise is used instead of collection promise.
    MongoDB tutorials and StackOverflow pages use to reuse the db connection promise instead of the collection promise.
    This approach solves some strange errors when multiple operations were performed in a certain order.

--- src/persistence/secondary/mongo-secondary-entity-manager.ts
+++ src/persistence/secondary/mongo-secondary-entity-manager.ts
@@ -1,5 +1,5 @@
 import { Entity } from '@antjs/ant-js/build/ant';
-import { Collection, MongoClient } from 'mongodb';
+import { Collection, Db, MongoClient } from 'mongodb';
 import { MongoBuildArgs } from '../../model/mongo-build-args';
 import { MongoModel } from '../../model/mongo-model';
 import { SecondaryEntityManager } from './secondary-entity-manager';
@@ -9,34 +9,34 @@ export class MongoSecondaryEntityManager<TEntity extends Entity> implements Seco
     return this._model;
   }
 
+  protected _db: Promise<Db>;
   protected _model: MongoModel<TEntity>;
-  private _collection: Promise<Collection>;
   private _client: Promise<MongoClient>;
 
-  constructor(model: MongoModel<TEntity>, args: MongoBuildArgs) {
+  public constructor(model: MongoModel<TEntity>, args: MongoBuildArgs) {
     this._model = model;
     this._client = MongoClient.connect(args.url);
-    this._collection = this._client.then((client) => client.db(args.dbName).collection(this._model.collectionName));
+    this._db = this._client.then((client) => client.db(args.dbName));
   }
 
   public async delete(id: string | number): Promise<any> {
-    return (await this._collection).findOneAndDelete({ id: id });
+    return (await this._getCollection()).findOneAndDelete({ id: id });
   }
 
   public async insert(entity: TEntity): Promise<any> {
-    return (await this._collection).insertOne(entity);
+    return (await this._getCollection()).insertOne(entity);
   }
 
   public async mDelete(ids: string[] | number[]): Promise<any> {
-    return (await this._collection).deleteMany({ id: { $in: ids } });
+    return (await this._getCollection()).deleteMany({ id: { $in: ids } });
   }
 
   public async mInsert(entities: TEntity[]): Promise<any> {
-    return (await this._collection).insertMany(entities);
+    return (await this._getCollection()).insertMany(entities);
   }
 
   public mUpdate(entities: TEntity[]): Promise<any> {
-    return this._collection.then((collection) => {
+    return this._getCollection().then((collection) => {
       return collection.bulkWrite(
         entities.map((entity) => {
           return { updateOne: { filter: { id: entity.id }, update: { $set: entity } } };
@@ -47,27 +47,28 @@ export class MongoSecondaryEntityManager<TEntity extends Entity> implements Seco
   }
 
   public async update(entity: TEntity): Promise<any> {
-    return (await this._collection).findOneAndUpdate(
+    return (await this._getCollection()).updateOne(
       { id: entity.id },
       { $set: entity },
-      {
-        upsert: true,
-      },
     );
   }
 
   public async getById(id: number | string): Promise<TEntity> {
-    return (await this._collection).findOne({ id: id });
+    return (await this._getCollection()).findOne({ id: id });
   }
 
   public async getByIds(ids: number[] | string[]): Promise<TEntity[]> {
-    return (await this._collection).find({ id: { $in: ids } }).toArray();
+    return (await this._getCollection()).find({ id: { $in: ids } }).toArray();
   }
 
   public async getByIdsOrderedAsc(ids: number[] | string[]): Promise<TEntity[]> {
-    return (await this._collection)
+    return (await this._getCollection())
       .find({ id: { $in: ids } })
       .sort({ id: 1 })
       .toArray();
+  }
+
+  protected _getCollection(): Promise<Collection<any>> {
+    return this._db.then((db) => db.collection(this.model.collectionName));
   }
 }
