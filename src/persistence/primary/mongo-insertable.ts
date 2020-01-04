69b95efd35025514ea39e80a5179f8b9b32ec31f
Roberto Pintos López <roberto@secture.com>
2020-01-04 12:09:38 +0100

    Added primary model manager.

---
+++ src/persistence/primary/mongo-insertable.ts
@@ -0,0 +1,19 @@
+import { Entity, PersistencyUpdateOptions } from '@antjs/ant-js';
+
+export interface SqlInsertable<TEntity extends Entity> {
+  /**
+   * Inserts an entity.
+   * @param entity Entity to be inserted.
+   * @param options persistency options.
+   * @returns Promise of entity inserted.
+   */
+  insert(entity: TEntity, options?: PersistencyUpdateOptions): Promise<any>;
+
+  /**
+   * Inserts multiple entities.
+   * @param entities Entities to be inserted.
+   * @param options persistency options.
+   * @returns Promise of entities inserted.
+   */
+  mInsert(entities: TEntity[], options?: PersistencyUpdateOptions): Promise<any>;
+}
---
+++ src/persistence/primary/primary-model-manager.ts
@@ -0,0 +1,7 @@
+import { Entity } from '@antjs/ant-js';
+import { PrimaryModelManager as AntJsPrimaryModelManager } from '@antjs/ant-js/build/persistence/primary/primary-model-manager';
+import { MongoInsertable } from './mongo-insertable';
+
+export interface PrimaryModelManager<TEntity extends Entity>
+  extends AntJsPrimaryModelManager<TEntity>,
+    MongoInsertable<TEntity> { }
